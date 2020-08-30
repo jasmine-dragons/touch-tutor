b2d3e95811322353c24d242cdcd7e23b20a15790
aidenszeto <aszeto35@gmail.com>
2020-08-30 09:36:11 -0700

    f

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+import pymongo
+import dns
+
+load_dotenv()
+app = Flask(__name__)
+
+@app.route('/')
+
+
+
+if __name__ == '__main__':
+    app.run()
