72398d062094f34661f18d563aa5127458a9dad6
PrateekS1917 <61072843+PrateekS1917@users.noreply.github.com>
2020-08-30 11:27:16 -0700

    s

--- app.py
+++ app.py
@@ -8,13 +8,21 @@ load_dotenv()
 URI = os.getenv('MONGO_URI')
 app = Flask(__name__)
 
+
 @app.route('/')
 def search():
     return render_template('search.html')
 
+
 @app.route('/results')
 def results():
     return render_template('results.html')
+
+
+@app.route('/job-posting')
+def job():
+    return render_template('job_post.html')
+
 
 if __name__ == '__main__':
     app.run()
