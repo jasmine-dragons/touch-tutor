3dc1d7da41cb53f77ca3c7544cf01fd261e9a928
aidenszeto <aszeto35@gmail.com>
2020-08-30 14:01:18 -0700

    search by subject

--- app.py
+++ app.py
@@ -31,7 +31,13 @@ def results():
         my_client = pymongo.MongoClient(URI)
         my_db = my_client['touch-tutor']
         my_col = my_db['postings']
+        tutor = my_col.find({'subject': str(subject)})
+        return tutor
 
+    output = get_tutor(subject, grade)
+
+    #tutor_name = loads(dumps(list(output)))
+    return render_template('results.html', name=output[0]["name"])
 
 
 
