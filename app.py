94f75a5bf28b92faa57bf8d3ca88f0db62dbd9db
Aroop Biswal <aroopbiswal@gmail.com>
2020-08-30 11:15:33 -0700

    f
    f

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session, redirect
 from dotenv import load_dotenv
 import os
 import pymongo
+import bcrypt
 import dns
 
 load_dotenv()
@@ -21,6 +22,23 @@ def search():
 @app.route('/results')
 def results():
     return render_template('results.html')
+
+
+@app.route('/signup')
+def results():
+    return render_template('signup.html')
+
+@app.route('/signup_tutor', methods=['POST', 'GET'])
+def results():
+    return render_template('signup_tutor.html')
+
+@app.route('/signup_student', methods=['POST', 'GET'])
+def results():
+    return render_template('signup_student.html')
+
+@app.route('/signup')
+
+
 
 if __name__ == '__main__':
     app.run()
